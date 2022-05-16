792508811e7719a69486ae7b726572954bb4e04c
Akash More <akashkmore1@gmail.com>
2022-05-16 22:50:28 +0530

    Display and hide mobile navbar

--- Working-With-JS-&-CSS/Webhosting-site-project/shared.js
+++ Working-With-JS-&-CSS/Webhosting-site-project/shared.js
@@ -5,6 +5,11 @@ const backdrop = document.querySelector(".backdrop");
 const buttons = document.querySelectorAll(".plan button");
 const noButton = document.querySelector(".modal__action--negative");
 
+const toggleButton = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
+
+const navItems = document.querySelectorAll(".mobile-nav__item a");
+
 const onClickHandler = () => {
   modal.classList.remove("modal-display");
   backdrop.classList.remove("backdrop-display");
@@ -17,6 +22,17 @@ const noButtonClickHandler = () => {
 
 const backdropClickHandler = () => {
   noButtonClickHandler();
+  mobileNav.classList.add("mobile-nav__display");
+};
+
+const toggleButtonClickHandler = () => {
+  mobileNav.classList.remove("mobile-nav__display");
+  backdrop.classList.remove("backdrop-display");
+};
+
+const navItemsClickHandler = () => {
+  backdrop.classList.add("backdrop-display");
+  console.log("click!");
 };
 
 for (let i = 0; i < buttons.length; i++) {
@@ -26,3 +42,9 @@ for (let i = 0; i < buttons.length; i++) {
 noButton.addEventListener("click", noButtonClickHandler);
 
 backdrop.addEventListener("click", backdropClickHandler);
+
+toggleButton.addEventListener("click", toggleButtonClickHandler);
+
+for (let i = 0; i < navItems.length; i++) {
+  navItems[i].addEventListener("click", navItemsClickHandler);
+}
